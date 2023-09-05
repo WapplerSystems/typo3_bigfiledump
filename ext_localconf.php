a29cd90579c3e3207e6d172ec24e7d6194cca89c
Sven Wappler <info@wappler.systems>
2023-09-05 15:52:37 +0200

    update

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,8 +2,4 @@
 
 use WapplerSystems\Bigfiledump\Controller\FileDumpController;
 
-if (!defined('TYPO3_MODE')) {
-    die ('Access denied.');
-}
-
 $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['dumpFile'] = FileDumpController::class . '::dumpAction';
